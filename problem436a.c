b4f331c00ae59f072c6e1b86678a6de27cf9cac6
Jonah Hooper <jonah.graham.hooper@gmail.com>
2013-09-24 19:46:23 +0200

    Removed inlines. Added feedback during estimation

--- problem436a.c
+++ problem436a.c
@@ -3,29 +3,29 @@
 #include <time.h>
 
 int main(){
-	int player2,iterations,count;
-	double sum,latest1,latest2;
-	srand(time(NULL));
-	player2=0;
-    scanf("%d",&iterations);
-    for (count=0;count<iterations;count++){
-           	sum=0;
-			while (sum<1){
-            	latest1=(double)rand()/(double)RAND_MAX;
-            	sum=sum+latest1;
-			}
-			while (sum<2){
-             	latest2=(double)rand()/(double)RAND_MAX;
-          		sum=sum+latest2;
-        	}
-			if (latest1<latest2){
-               	player2++;
-			}
-			printf("%20.18f \n",player2/(double)count);
-	}
-	printf("\n\n %20.18f \n\n",player2/(double)iterations);
-	system("pause");
-	return 0;
+  int player2,iterations,count;
+  double sum,latest1,latest2;
+  srand(time(NULL));
+  player2=0;
+  scanf("%d",&iterations);
+  for (count=0;count<iterations;count++){
+    sum=0;
+    while (sum<1){
+      latest1=(double)rand()/(double)RAND_MAX;
+      sum=sum+latest1;
+    }
+    while (sum<2){
+      latest2=(double)rand()/(double)RAND_MAX;
+      sum=sum+latest2;
+    }
+    if (latest1<latest2){
+      player2++;
+    }
+    printf("%20.18f \n",player2/(double)count);
+  }
+  printf("\n\n %20.18f \n\n",player2/(double)iterations);
+  system("pause");
+  return 0;
 }
 
 
--- problem436b.c
+++ problem436b.c
@@ -2,29 +2,33 @@
 #include <stdio.h>
 #include <time.h>
 
+#define ANSWERE_FORMAT "%.15Lf\n"
+
 typedef char bool;
 
-inline bool s_over_one (double s) {
+bool s_over_one (double s) {
   return (s > 1.0);
 }
 
-inline bool s_over_two (double s) {
+bool s_over_two (double s) {
   return (s > 2.0);
 }
 
-inline double random_double () {
+double random_double () {
   return ( (double) rand () / (double) RAND_MAX );
 }
 
-inline long double probability_player_two_wins (long number_simulations) {
+long double probability_player_two_wins (long max_iterations, long iterations_per_feedback) {
   double S = 0.0;
   double player_one_random = 0.0;
   double player_two_random = 0.0;
 
   long iterations = 0;
   long player_two_score = 0;
+  
+  time_t start_time = clock ();
 
-  while (iterations < number_simulations) {
+  while (iterations < max_iterations) {
     while (!s_over_one (S)) {
       player_one_random = random_double ();
       S += player_one_random;
@@ -34,15 +38,24 @@ inline long double probability_player_two_wins (long number_simulations) {
       player_two_random = random_double ();
       S += player_two_random;
     }
+
     if (player_two_random > player_one_random) player_two_score++;
     S = 0.0;
     iterations++;
+    if (iterations % iterations_per_feedback == 0) {
+      printf ("Iterations: %ld\tRemaining: %ld\tEstimate: ",iterations, max_iterations - iterations);
+      printf (ANSWERE_FORMAT, (long double) player_two_score / (long double) iterations); 
+    }
   }
-  return (long double) player_two_score / (long double) number_simulations;
+
+  time_t end_time = clock ();
+  printf ("Time taken %lfS\n", (double)(end_time - start_time)/1000000.0);
+  return (long double) player_two_score / (long double) max_iterations;
 }
 
 int main () {
-  const long NUMBER_REQUIRED = 10000000000000;
+  const long NUMBER_REQUIRED = 10000000000;
+  const long ITERATIONS_PER_FEED_BACK = 5000000;
   srand (time(NULL));
-  printf ("%.15Lf\n", probability_player_two_wins (NUMBER_REQUIRED));
+  printf (ANSWERE_FORMAT, probability_player_two_wins (NUMBER_REQUIRED, ITERATIONS_PER_FEED_BACK));
 }
